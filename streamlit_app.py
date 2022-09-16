98a449b0662b4ee2bd75e29a61f3b97de79b8169
Sunainasram <71332958+Sunainasram@users.noreply.github.com>
2022-09-16 19:03:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,69 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[ ]:
-
-
 import pandas as pd
-
-
-# In[ ]:
-
-
 orvm_data=pd.read_csv("C:/Users/sunaina.s.ram/Downloads/ORVM Dataset.csv")
 
 
-# In[ ]:
 
 
 orvm_data
 
-
-# In[ ]:
 
 
 orvm_data.info()
 
 
-# In[ ]:
-
-
-#data={'cust_rating':['Bad','Average','Good']}
-
-
-
-
-
-#df_ordinal=pd.DataFrame(data)
-
-
-# In[ ]:
-
-
-#df_ordinal
-
-
-# In[ ]:
-
 
 orvm_data
 
 
-# In[ ]:
+
 
 
 orvm_data.head()
 
-
-# In[ ]:
-
-
 orvm_data=orvm_data.dropna()
 orvm_data
-
-
-# In[ ]:
-
 
 # Import label encoder
 from sklearn.preprocessing import LabelEncoder
@@ -74,66 +34,19 @@ label_encoder = LabelEncoder()
 # Encode labels in column 'species'.
 orvm_data['Vendor']= label_encoder.fit_transform(orvm_data['Vendor'])
 
-
-# In[ ]:
-
-
 orvm_data['Vendor'].unique()
-
-
-# In[ ]:
-
-
 orvm_data['Vehicle Type ']= label_encoder.fit_transform(orvm_data['Vehicle Type '])
 orvm_data['Vehicle Type ']
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 orvm_data['ORVM Type']=label_encoder.fit_transform(orvm_data['ORVM Type'])
-
-
-# In[ ]:
-
-
 orvm_data['ORVM Type']
-
-
-# In[ ]:
-
 
 orvm_data['Auto Dimming ']=label_encoder.fit_transform(orvm_data['Auto Dimming '])
 orvm_data['Auto Dimming ']
-
-
-# In[ ]:
-
-
 orvm_data.dtypes
-
-
-# In[ ]:
-
-
 orvm_data['Vendor'].unique()
-
-
-# In[ ]:
-
-
 orvm_data['Vendor'].value_counts()
-
-
-# In[ ]:
-
-
 orvm_data['Vendor'].value_counts().sum()
 
 
@@ -215,19 +128,6 @@ from sklearn import metrics
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=100)
 
-
-# model = svm.SVC() #select the algorithm
-# model.fit(x_train,y_train) # we train the algorithm with the training data and the training output
-# prediction=model.predict(x_test) #now we pass the testing data to the trained algorithm
-# print('The accuracy of the SVM is:',metrics.accuracy_score(prediction,y_test))#now we check the accuracy of the algorithm. 
-# #we pass the predicted output by the model and the actual output
-
-# model = LogisticRegression()
-# model.fit(x_train,y_train)
-# prediction=model.predict(x_test)
-# print('The accuracy of the Logistic Regression is',metrics.accuracy_score(prediction,y_test))
-
-# In[ ]:
 
 
 modeld=DecisionTreeClassifier(criterion='gini', random_state=100,max_leaf_nodes=15)
